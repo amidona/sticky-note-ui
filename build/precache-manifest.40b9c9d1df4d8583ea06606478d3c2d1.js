956242a1f9e02ca6299cb94d2950ddddaf9701d1
Allison Amidon <93014228+amidona@users.noreply.github.com>
2022-07-11 14:41:04 -0500

    Added Node.js

---
+++ build/precache-manifest.40b9c9d1df4d8583ea06606478d3c2d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5be267b43d15deff0735",
+    "url": "%3Chttps://amidona/github.io/sticky-note-ui%3E/static/css/main.d3b62a9d.chunk.css"
+  },
+  {
+    "revision": "5be267b43d15deff0735",
+    "url": "%3Chttps://amidona/github.io/sticky-note-ui%3E/static/js/main.a40f148d.chunk.js"
+  },
+  {
+    "revision": "355aa89974516c14e734",
+    "url": "%3Chttps://amidona/github.io/sticky-note-ui%3E/static/js/runtime~main.a81e89de.js"
+  },
+  {
+    "revision": "59fa5faa0d43ca05c280",
+    "url": "%3Chttps://amidona/github.io/sticky-note-ui%3E/static/js/2.da757658.chunk.js"
+  },
+  {
+    "revision": "53f43283b4a4158bcbe88d64c14ba4a6",
+    "url": "%3Chttps://amidona/github.io/sticky-note-ui%3E/index.html"
+  }
+];
